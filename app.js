5aa55e1d31a97225ce3678a330f93d3b09bbc21a
Saurav Verma <166201391+Saurav0055@users.noreply.github.com>
2026-04-27 17:29:58 +0530

    Removed invalid dependency

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ import { removeunwantedverifyaccount } from './Services/removeunwantedverifyacco
 
 
 export const app = express();
-config({ path: "./config/config.env" });
+config();
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
